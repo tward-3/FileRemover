9c6fa9a4dc1d018cabec4e7146ea1e5e62e00f83
Thomas Ward <59423939+tward-3@users.noreply.github.com>
2024-02-27 21:10:33 -0600

    Create FileRemover.ps1
    Main PowerShell code

---
+++ FileRemover.ps1
@@ -0,0 +1,25 @@
+# Path to text file
+$textFilePath = ""
+ 
+# Path to directory
+$directoryPath = ""
+ 
+# Read the list of filenames from the text file 
+$filenamesToKeep = Get-Content $textFilePath
+ 
+# Get a list of all files in the directory
+$allFiles = Get-ChildItem -Path $directoryPath
+ 
+# Loop through each file in the directory
+foreach ($file in $allFiles) {
+    $filename = $file.Name.Substring(0, 9)
+    Write-Host "Checking filename $($filename)"
+   
+    # Check if the filename is in the list of filenames extracted from the text file
+    if ($filenamesToKeep -contains $filename) {
+        Write-Host "Filename $($filename) found in the text file."
+    } else {
+        Write-Host "Filename $($filename) not found in the text file. Removing file $($file.FullName)."
+        Remove-Item -Path $file.FullName -Force
+    }
+}
